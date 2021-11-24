86b19899d6ede4adab186a7d8456d5e2316adf4f
Luca Lamerichs <luca.lamerichs@rwth-aachen.de>
2021-11-24 21:45:30 +0100

    Specify range

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from construction_site import ConstructionSite
 
-# ConstructionSite('2021-04-03').progressAt() -> datetime.datetime<2021, 4, 3>
-print(ConstructionSite('2021-04-04').progressAt())
+# ConstructionSite('2021-04-03').progressAt('2021-04-13') -> [datetime.datetime<2021, 4, 3>, datetime.datetime<2021, 4, 4>, ..., datetime.datetime<2021, 4, 13>]
+print(ConstructionSite('2021-04-04').progressAt('2021-04-13'))
